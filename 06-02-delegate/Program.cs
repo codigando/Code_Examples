72a52f4b4475c51d188b12d6591bcdf8007dd814
psburl <wesleyburlani@gmail.com>
2017-02-06 19:08:22 -0200

    add delegate example

---
+++ 06-02-delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication{
+    
+    public class Program{
+
+        public delegate bool Check(object value);
+
+        public static bool IsNull(object value){
+            return value == null;
+        }
+
+        public static void Main(string[] args){
+
+            string genericObject = "2";
+
+            Check myObjectIsNull = IsNull;
+
+            if(myObjectIsNull(genericObject))
+                Console.WriteLine("is null!");
+        }
+    }
+}
